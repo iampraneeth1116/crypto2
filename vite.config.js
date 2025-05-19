36fd175fbb80c85321fc560da61f3dacce40cac1
Praneeth <iampraneeth1116@gmail.com>
2025-05-19 21:15:47 +0530

    fga

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/crypto2/', // Update this to match your repository name
+  base: './', // Add this line
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
